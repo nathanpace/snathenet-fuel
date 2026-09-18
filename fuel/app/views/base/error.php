e0c6467cf590fbc894256e4076484a146ca9ce2f
Nathan Pace <github@snathe.net>
2026-09-18 18:42:04 +0100

    - Created new base views for generic error messages - Better handling of InvalidPathException on download files - Changed download route so it's now directory style

---
+++ fuel/app/views/base/error.php
@@ -0,0 +1,9 @@
+<div class="row">
+	<div class="col-md-12">
+		<h1><?=$title;?> <small>We appear to have run into a problem.</small></h1>
+		<div class="para">
+			<?=$message;?>
+		</div>
+	</div>
+
+</div>
